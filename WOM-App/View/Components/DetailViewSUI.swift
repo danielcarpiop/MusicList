7c44cd8227f3013d2d7c42ffbdd8a8f0b0cfdcb0
Daniel Carpio <danielcarpiop@outlook.com>
2024-03-28 09:11:40 -0300

    feature/SUI-implementation

---
+++ WOM-App/View/Components/DetailViewSUI.swift
@@ -0,0 +1,9 @@
+//import SwiftUI
+//
+//struct DetailViewSUI: View {
+//    var model: ViewModel?
+//    
+//    var body: some View {
+//        Image(label: model?.name, attributes: model?.name)
+//    }
+//}
